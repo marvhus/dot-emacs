346bf44e57082521e5d4b7ea382a9f0365876571
Martin <marvahus@proton.me>
2022-10-21 21:05:43 +0200

    Added magit

--- init.el
+++ init.el
@@ -157,5 +157,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rust-mode undo-fu undo-tree dap-mode lsp-ivy helm-lsp company lsp-treemacs flycheck lsp-ui lsp-mode auto-complete evil))
+   '(magit rust-mode undo-fu undo-tree dap-mode lsp-ivy helm-lsp company lsp-treemacs flycheck lsp-ui lsp-mode auto-complete evil))
  '(warning-suppress-types '((lsp-mode))))
