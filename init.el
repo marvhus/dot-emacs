11d99e8bb518355235a6836fe78a027b7d93f277
Martin <marvahus@proton.me>
2022-11-24 22:38:58 +0100

    Theme updates
    moved away from submodule to MELPA package for grubber-darker

--- init.el
+++ init.el
@@ -139,8 +139,6 @@
 
 ;;;;;;;;;;;;
 ;; https://github.com/rexim/gruber-darker-theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/gruber-darker-theme")
-;; use M-x customize-themes to change theme
 (load-theme 'gruber-darker t)
 ;;;;;;;;;;;
 
