bc59d93efcb8a693a41eaae8d0a0ebb1de03f09a
Martin <marvahus@proton.me>
2022-12-14 23:07:19 +0100

    update

--- init.el
+++ init.el
@@ -16,6 +16,8 @@
 ;;(ac-config-default)
 ;;(auto-complete-mode)
 
+(exec-path-from-shell-initialize)
+
 ;; evil
 (require 'evil)
 (evil-mode 1)
@@ -24,6 +26,7 @@
 (setq lsp-keymap-prefix "C-c l")
 (require 'lsp-mode)
 ;(require 'lsp-clients)
+;(setq lsp-zig-zls-executable "/opt/zls/bin/zls")
 (add-hook 'prog-mode-hook #'lsp)
 
 ;; company
@@ -35,6 +38,9 @@
 (require 'helm-git-grep)
 (require 'helm-ls-git)
 (require 'helm-org)
+
+;; C stuff
+(setq-default c-basic-offset 4)
 
 ;; ox-hugo
 (use-package ox-hugo
@@ -50,7 +56,11 @@
 (setq calendar-week-start-day 1)
 (setq column-number-mode t)
 (setq scroll-bar-mode 0)
-(setq backup-directory-alist `(("." . "~/.saves")))
+;; NO I DO NOT WANT THEESE
+;; (setq backup-directory-alist `(("." . "~/.saves")))
+(setq make-backup-files nil) ; stop creating ~ files
+(setq create-lockfiles nil) ; stop creating .# files
+(setq make-backup-files nil) ; stop creating # files
 
 ;; server
 ;(load "server")
@@ -62,6 +72,16 @@
 ;; Odin Mode -- https://github.com/mattt-b/odin-mode
 (require 'odin-mode)
 (add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-mode))
+
+;; With odin-mode (https://github.com/mattt-b/odin-mode) and lsp-mode already added to your init.el of course!.
+(setq-default lsp-auto-guess-root t) ;; if you work with Projectile/project.el this will help find the ols.json file.
+(defvar lsp-language-id-configuration '((odin-mode . "odin")))
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection "~/src/thirdparty/ols/ols")
+                  :major-modes '(odin-mode)
+                  :server-id 'ols
+                  :multi-root t)) ;; This is just so lsp-mode sends the "workspaceFolders" param to the server.
+(add-hook 'odin-mode-hook #'lsp)
 
 ;; Drag Stuff
 (add-to-list 'load-path "~/.emacs.d/drag-stuff/")
@@ -93,14 +113,24 @@
 (global-set-key (kbd "C-c r") 'recompile)
 (global-set-key (kbd "C-c c") 'compile)
 
+(global-set-key (kbd "C-c C-d x") 'dired-do-shell-command)
+(global-set-key (kbd "C-c C-d z") 'dired-do-chmod)
+(global-set-key (kbd "C-c C-d v") 'dired-diff)
+(global-set-key (kbd "C-c C-d c") 'dired-other-window)
+
 (global-set-key [C-w v] 'split-window-right)
 (global-set-key [C-w s] 'split-window-below)
 (global-set-key [C-w q] 'delete-window)
 
-(global-set-key (kbd "<C-up>") 'shrink-window)
-(global-set-key (kbd "<C-down>") 'enlarge-window)
-(global-set-key (kbd "<C-left>") 'shrink-window-horizontally)
-(global-set-key (kbd "<C-right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "<C-up>")		'shrink-window)
+(global-set-key (kbd "<C-down>")	'enlarge-window)
+(global-set-key (kbd "<C-left>")	'shrink-window-horizontally)
+(global-set-key (kbd "<C-right>")	'enlarge-window-horizontally)
+
+(global-set-key (kbd "C-S-<up>")	'windmove-up)
+(global-set-key (kbd "C-S-<down>")	'windmove-down)
+(global-set-key (kbd "C-S-<left>")	'windmove-left)
+(global-set-key (kbd "C-S-<right>") 'windmove-right)
 
 (define-key evil-normal-state-map (kbd "u") 'undo-fu-only-undo)
 (define-key evil-normal-state-map (kbd "C-r") 'undo-fu-only-redo)
@@ -111,6 +141,26 @@
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 
+;; Multiple Cursors
+
+;; Do What I mean
+(global-set-key (kbd "C-M-j") 'mc/mark-all-dwim)
+
+;; Edit lines
+(global-set-key (kbd "C-M-c") 'mc/edit-lines)
+
+;; Mark
+(global-set-key (kbd "C-M-,") 'mc/mark-previous-like-this) 
+(global-set-key (kbd "C-M-.") 'mc/mark-next-like-this) 
+
+;; Unmark
+(global-set-key (kbd "C-M-n") 'mc/unmark-previous-like-this) 
+(global-set-key (kbd "C-M-m") 'mc/unmark-next-like-this) 
+
+;;; Skip
+(global-set-key (kbd "C-M-<") 'mc/skip-to-previous-like-this)
+(global-set-key (kbd "C-M->") 'mc/skip-to-next-like-this)
+
 ;; Scrolling and tab width
 (setq-default tab-width 4)
 (setq scroll-margin 20)
@@ -118,6 +168,9 @@
 
 ;; Hide Tool Bar
 (tool-bar-mode -1)
+
+;; Good File Finding
+(ivy-mode 1)
 
 ;; set type of line numbering (global variable)
 (setq display-line-numbers-type 'relative) 
@@ -192,7 +245,7 @@
 ;;(set-default-font "-b&h-lucida console-medium-r-semi condensed--0-0-0-0-m-0-ascii-0")
 ;;(set-default-font "-*-Fixedsys-r-*-*-*-*-*-*-*-*-*-ansi-")
 ;;(set-face-attribute 'default nil font "Anonymous Pro-14")
-;(set-face-attribute 'default nil :font "Consolas-13")
+(set-face-attribute 'default nil :font "Consolas-13")
 
 ;(set-face-foreground 'font-lock-builtin-face        "lightgreen")
 ;;(set-face-foreground 'font-lock-constant-face      "darkred")
@@ -211,6 +264,7 @@
 ;      c++-font-lock-keywords c++-font-lock-keywords-2
 ;      lisp-font-lock-keywords lisp-font-lock-keywords-2)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -218,8 +272,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("ba4ab079778624e2eadbdc5d9345e6ada531dc3febeb24d257e6d31d5ed02577" default))
+ '(gdb-many-windows t)
+ '(gdb-show-main t)
  '(package-selected-packages
-   '(gruber-darker-theme org-bullets use-package ox-hugo helm-org helm-ls-git helm-git-grep helm-cmd-t multiple-cursors smex nim-mode zig-mode magit rust-mode undo-fu undo-tree dap-mode lsp-ivy helm-lsp company lsp-treemacs flycheck lsp-ui lsp-mode auto-complete evil))
+   '(haskell-mode exec-path-from-shell gruber-darker-theme org-bullets use-package ox-hugo helm-org helm-ls-git helm-git-grep helm-cmd-t multiple-cursors smex nim-mode zig-mode magit rust-mode undo-fu undo-tree dap-mode lsp-ivy helm-lsp company lsp-treemacs flycheck lsp-ui lsp-mode auto-complete evil))
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -227,3 +283,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
